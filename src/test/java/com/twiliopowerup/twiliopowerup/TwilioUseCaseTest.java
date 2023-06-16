4fde96ed99ac506bc7c4038f37271706af0e4d11
Sofia Millan <sofia.millan@pragma.com.co>
2023-06-16 14:22:07 -0500

    Adding jacoco

--- src/test/java/com/twiliopowerup/twiliopowerup/TwilioUseCaseTest.java
+++ src/test/java/com/twiliopowerup/twiliopowerup/TwilioUseCaseTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class TwilioUseCaseTest {
+class TwilioUseCaseTest {
      ITwilioPersistencePort twilioPersistencePort;
      ITwilioServicePort twilioServicePort;
      MessageModel messageModel;
