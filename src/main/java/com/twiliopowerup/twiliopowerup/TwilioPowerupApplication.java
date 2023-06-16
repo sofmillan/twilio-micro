7dd76cb6421a349490dd00857c0f501e13a21d73
Sofia Millan <sofia.millan@pragma.com.co>
2023-06-16 14:13:37 -0500

    Deleting comments

--- src/main/java/com/twiliopowerup/twiliopowerup/TwilioPowerupApplication.java
+++ src/main/java/com/twiliopowerup/twiliopowerup/TwilioPowerupApplication.java
@@ -2,7 +2,7 @@ package com.twiliopowerup.twiliopowerup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class TwilioPowerupApplication {
